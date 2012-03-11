2d20dccf27d873d695a8d60bc69a4d8b8b948b3d
Steven Skoczen <steven@quantumimagery.com>
2012-03-10 19:55:57 -0800

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import *
 
-env.PROJECT_NAME = "artechetype"
+env.PROJECT_NAME = "weexist"
 env.GITHUB_USER = "skoczen"
 env.GITHUB_REPO = env.PROJECT_NAME
 env.VIRTUALENV_NAME = env.PROJECT_NAME
